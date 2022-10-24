018e09da4f59ffcca42ff8f59192ae953730cf7e
Kieran Best <kb806@canterbury.ac.uk>
2022-10-24 15:21:09 +0000

    Trying to pass server tests, came across 2 common problems, "list indices must be integers or slices, not str convert to javascript" and "'list' object has no attribute 'get'".

--- server/server.js
+++ server/server.js
@@ -1,91 +1,90 @@
 const express = require('express')
 const app = express()
 const port = 8000
-
-
-const cors = require('cors');
+app.use(express.json());
 const { response } = require('express');
 
-//const path = __dirname + '../client';
-//app.use(express.static(path))
-
-app.use(cors());
-app.use(express.json());
-
+//Cors stuff
+//const cors = require('cors');
+//app.use(cors());
 //var corsOptions = {
 //    origin: "https://8000-kieranbest-frameworksan-rkxnupaltqd.ws-eu71.gitpod.io"}
 //app.use(cors(corsOptions))
 
+//Creating a link with the client
+//const path = __dirname + '../client';
+//app.use(express.static(path))
+
+//Object of Items
 Item = [{
     "id": 1,
     "user_id": "Sting",
-    "keywords": "String",
+    "keywords": ["String","another string"],
     "description": "string",
     "lat": 2,
     "lon": 1,
     "date_from": "2021-11-22T08:22:39.067408",
 }]
 
-
-function latitude(){(Math.random() * (70*2)) - 70}
-function longitude(){(Math.random() * (180*2)) - 180}
-var auto_id=1;
-
+//Functions to create automatic values 
+function latitude(){
+    var latitude = (Math.random() * (70*2)) - 70
+    return latitude}
+function longitude(){
+    var longitude = (Math.random() * (180*2)) - 180
+    return longitude}
 function auto_Id(){
-    var id = auto_id++;
-    return id;
-}
-
-function  iso_date(){
+    var id = Item.length+1
+    return id;}
+function  new_date(){
     //https://stackoverflow.com/questions/1531093/how-do-i-get-the-current-date-in-javascript
-    let today = new Date().toISOString().slice(0, 10)
-    return today;
-}
+    let today = new Date().toISOString()
+    return today;}
 
 filteredItem = []
 
+//Get requests
 app.get('/', (req, res) => {
 //    res.sendFile(path + "vue.html")
     res.status(200).send('<html><body><h1>Your HTML text<h1></body></html>')
 })
-
-app.post('/item', (req,res) => {
-    req.body={
-        id:auto_Id(),
-        user_id:req.body.user_id,
-        keywords:req.body.keywords,
-        description:req.body.description,
-    
-        
-    }
-    Item.push(req.body)
-    res.status(201).json(Item)
-    
-})
-
 app.get('/items', (req,res) => {
-    for (i=0;Item.length;i++){
-        filteredItem = Item.itemId[i], Item.keywords[i]
-        console.log(filteredItem)
-    }
-    //res.json(filteredItem)
-    //Item = Item.slice(1,2,3)
     res.status(200).json(Item)
 })
-
 app.get('/item/:id', (req,res) => {
     filteredItem = Item.filter(element => element.id === parseFloat(req.params.id))
     if (filteredItem.length === 0){        
         res.status(404).json("Item not found")
     }
     else{        
-        res.json(filteredItem)
-        res.status(200).json("Successful Operation")
+        res.status(200).json(filteredItem)
     }
 })
 
+//Post request
+app.post('/item', (req,res) => {
+    req.body={
+        id:auto_Id(),
+        user_id:req.body.user_id,
+        keywords:req.body.keywords,
+        description:req.body.description,
+        lat:latitude(),
+        lon:longitude(),
+        date_from:new_date(),
+    }
+    console.log(latitude())
+    console.log(longitude())
+
+    Item.push(req.body)
+    console.log(req.body)
+    res.status(201).json(Item)
+    console.log(Item)
+})
+
+
+//Delete request
 app.delete('/item/:id', (req,res) => {
-    filteredItem = Item.filter(element => element.id === parseFloat(req.params.id))
+    filteredItem = Item.filter(element => element.id === parseInt(req.params.id))
     
     if (filteredItem.length === 0){        
         res.status(404).json("Item not found")
@@ -96,6 +95,7 @@ app.delete('/item/:id', (req,res) => {
     }
 })
 
+//Listens on port 8000
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
